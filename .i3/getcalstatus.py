c3303045fd716414e4b91f77552f7aa62ec05524
Peter Vilim <peterlvilim@gmail.com>
2014-10-12 22:11:18 -0500

    cal

--- .i3/getcalstatus.py
+++ .i3/getcalstatus.py
@@ -9,7 +9,7 @@ stop_if_already_running(os.path.basename(__file__));
 time.sleep(30);
 try:
     result=commands.getstatusoutput("gcalcli --nc --ignore-started agenda \"`date`\" | head -2 | tail -1")
-    if "Error" not in result[1]:
+    if "Error" not in result[1] and "Exception" not in result[1]:
         f = open("/home/pvilim/.i3/cal.status", "w");
         f.write(result[1]);
         f.close();
